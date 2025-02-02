566602114e1e99ed59de24fdc0136092513d22e4
Name <lasagna@garfunkles.space>
2025-02-02 14:51:22 +0000

    Feat: Setup gitolite

--- modules/nixos/servers/git.nix
+++ modules/nixos/servers/git.nix
@@ -5,14 +5,31 @@
   ...
 }:
 {
+  # Setup gitolite
   services.gitolite = {
     enable = true;
+    user = "git";
     dataDir = "/srv/git";
 
     # Configure gitolite
-    # extraGitoliteRc
+    extraGitoliteRc = ''
+      $RC{SITE_INFO} = "Name's Git server! Running Gitolite and uysdfeuy <3";
+      push( @{$RC{ENABLE}}, 'Kindergarten' ); # WARN: DO NOT REMOVE. STOPS USERS BEING DUMB.
+      push( @{$RC{ENABLE}}, 'set-default-roles' ); # Enables us to set default roles.
+      push( @{$RC{ENABLE}}, 'create' ); # Enables us to set default roles.
+      push( @{$RC{ENABLE}}, 'fork' ); # Enables us to set default roles.
+      push( @{$RC{ENABLE}}, 'mirror' ); # Enables us to set default roles.
+      push( @{$RC{ENABLE}}, 'readme' ); # Enables us to set default roles.
+      push( @{$RC{ENABLE}}, 'sskm' ); # Enables us to set default roles.
+      push( @{$RC{ENABLE}}, 'D' ); # Enables us to set default roles.
+
+      # @{$RC{ENABLE}} = grep { $_ ne 'desc' } @{$RC{ENABLE}}; # disable the command/feature
+    '';
 
     # Sets the initial adminPubkey
-    adminPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAy6DQPAN9Qo6k0VpD10kdV+fuHqofVKh/D4U2GFyXF7 Name@navi";
+    adminPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPXLaWnHGxCOd9g7KwkISKeXBpTKy7mdulKdjP/pRezm Name@navi";
   };
+
+  # WARN: This is super important. Without adding it to the ssh user-group it can NOT use ssh.
+  users.users.git.extraGroups = [ "allowed-ssh" ];
 }
